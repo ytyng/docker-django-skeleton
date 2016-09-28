ab44b53e12e4f82604d3843c3e639a2e82dbfc00
yotsuyanagi <yotsuyanagi@torico-tokyo.com>
2016-09-28 09:26:46 +0900

    nginx conf 半動的

--- docker/docker-01-build.sh
+++ docker/docker-01-build.sh
@@ -4,4 +4,6 @@ cd $(dirname ${BASH_SOURCE:-$0})
 
 . _settings.sh
 
+python -c "import os; print(open('nginx-app-template.conf').read() % os.environ)" > nginx-app.conf
+
 ${DOCKER_COMMAND} build -t ${APP_NAME} .
